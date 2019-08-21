c47c856630606a9bfb4e4b5b14f7a383b934fb57
HUECTRUM <plyusnovdmitrii@gmail.com>
2019-08-21 18:19:20 +0300

    basic logging for player select state

--- src/main/java/com/bacon/selectors/player/DummyPlayerSelector.java
+++ src/main/java/com/bacon/selectors/player/DummyPlayerSelector.java
@@ -2,14 +2,17 @@ package com.bacon.selectors.player;
 
 import com.bacon.characters.dummy.DummyOne;
 import com.bacon.player.Player;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import static com.bacon.player.Player.fromCharacter;
 
 @Component
+@Slf4j
 public class DummyPlayerSelector implements PlayerSelector {
     @Override
     public Player selectPlayer() {
+        log.info("Player select called");
         return fromCharacter(new DummyOne());
     }
 }
--- src/main/java/com/bacon/statemachine/resolvers/CharacterSelectionResolver.java
+++ src/main/java/com/bacon/statemachine/resolvers/CharacterSelectionResolver.java
@@ -2,10 +2,12 @@ package com.bacon.statemachine.resolvers;
 
 import com.bacon.holders.GameInfoHolder;
 import com.bacon.selectors.player.PlayerSelector;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class CharacterSelectionResolver {
     @Autowired
     private PlayerSelector playerSelector;
@@ -13,10 +15,12 @@ public class CharacterSelectionResolver {
     public void selectPlayers(GameInfoHolder gameInfoHolder) {
         gameInfoHolder.playerOne = playerSelector.selectPlayer();
         gameInfoHolder.playerTwo = playerSelector.selectPlayer();
-        
+
         gameInfoHolder.field.setPlayers(
                 gameInfoHolder.playerOne.character.displayName(),
                 gameInfoHolder.playerTwo.character.displayName()
         );
+
+        log.info("Player select finished. Field {}", gameInfoHolder.field);
     }
 }
